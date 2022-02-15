e35694ef9ac235189184e18dad9c064a4f7a65b2
IB-SKILLS <97691735+IB-SKILLS@users.noreply.github.com>
2022-02-15 11:01:55 +0500

    Create client-ad.ps1

---
+++ Scripts/client-ad.ps1
@@ -0,0 +1,16 @@
+# Разрешаем использование испольняемого файла для всех пользователей
+# Set-ExecutionPolicy RemoteSigned
+# -A
+
+# Вводим переменные
+$Username = 'user-agent'
+$Password = 'xxXX1234'
+$ldap_path = 'OU=Computers,OU=DemoOffice,DC=demo,DC=lab'
+
+# Настройка -credential
+[SecureString]$Securepassword = $Password | ConvertTo-SecureString -AsPlainText -Force 
+$credential = New-Object System.Management.Automation.PSCredential -ArgumentList $Username, $Securepassword
+
+# Добавляем пользователя в домен и перезагружаем комьютер
+Add-Computer -DomainName demo.lab -NewName $Username -OUPath $ldap_path -Credential $credential
+Restart-Computer -Force
